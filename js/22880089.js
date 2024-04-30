50d6267876facbf47fd8b72835631ae6f7f2eae4
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-04-30 13:54:39 +0700

    update sendMail

--- js/22880089.js
+++ js/22880089.js
@@ -1,5 +1,6 @@
 
 const API = 'https://web1-api.vercel.app/api';
+const AUTHENTICAT_API = 'https://web1-api.vercel.app/user';
 // get list product
 async function load_data(request, templateId, viewId){
     const response = await fetch(`${API}/${request}`);
@@ -9,4 +10,20 @@ async function load_data(request, templateId, viewId){
     var context = {data: getData};
     var view = document.getElementById(viewId);
     view.innerHTML = template(context);
+}
+
+async function getAuthenticateToken(username,password){
+    let response = await fetch(`${AUTHENTICAT_API}/authenticate`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({username, password})
+    });
+    let result = await response.json();
+    if(response.status == 200){
+       return result.token;
+    }
+    throw new Error(result.message);
 }
