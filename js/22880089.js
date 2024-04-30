f355f5416a8d8d27243749c36b2d5ee6d8f3038e
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-04-30 17:24:45 +0700

    update

--- js/22880089.js
+++ js/22880089.js
@@ -27,3 +27,70 @@ async function getAuthenticateToken(username,password){
     }
     throw new Error(result.message);
 }
+
+async function login(e){
+    e.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    document.getElementById('errorMessage').innerText = '';
+
+    try{
+        let token = await getAuthenticateToken(username, password);
+        if(token){
+            localStorage.setItem('token', token);
+            document.getElementsByClassName('btn-close')[0].click();
+            displayControl();
+        }
+    } catch (error){
+        document.getElementById('errorMessage').innerText = error;
+        displayControl(false);
+    }
+}
+function displayControl(isLogin = true){
+    let linkLogin = document.getElementsByClassName('linkLogin');
+    let linkLogout = document.getElementsByClassName('linkLogout');
+    let displayLogin = 'none';
+    let displayLogout = 'block';
+    if(!isLogin){
+        displayLogin = 'block';
+        displayLogout = 'none';
+    }
+    for(let i = 0; i < 2; i++){
+        linkLogin[i].style.display = displayLogin;
+        linkLogout[i].style.display = displayLogout;
+    }
+
+    let leaveComment = document.getElementById('leave-comment');
+    if(leaveComment){
+        leaveComment.style.display = displayLogout;
+    }
+}
+
+async function checkLogin(){
+    let isLogin = await verifyToken();
+    displayControl(isLogin);
+}
+
+async function verifyToken(){
+    let token = localStorage.getItem('token');
+    if(token){
+        let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer` + token
+            }
+
+        });
+        if(response.status === 200){
+            return true;
+        }
+    }
+    return false;
+}
+
+function logout(){
+    localStorage.clear();
+    displayControl(false);
+}
